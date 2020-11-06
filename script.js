98a91935cbc839bc6b48550fa85e8b521394d904
Glitch (cautious-chocolate-crop) <none>
2020-11-06 02:05:30 +0000

    🗼🍴 Updated with Glitch

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+let disOld = 2;
 
+$("button").click(function(){
+  let name = $(".name").val();
+  let birthDate = $(".year").val();
+  disOld = 2020 - (Number(birthDate));
+  if (disOld < 1000){
+    $(".message").text("Hey, you. Yeah, you, "+ name +". yeah you're "+ disOld +". As in you're a babyyyyy compared to meeeee :))))")
+  }
+  else if (disOld > 1000 ){    
+  $(".message").text("Hey, you. Yeah, you, "+ name +". yeah you're "+ disOld +". Oh, you're an oldie, even older than me. Uh I guess, thank you for using our services most esteemed customer?")}
+ 
+});
